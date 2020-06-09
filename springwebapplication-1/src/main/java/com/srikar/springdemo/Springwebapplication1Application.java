0a35e13fe6cc61ac32dea6814d8a91d5ae6df65c
srikar <srikark999@gmail.com>
2020-06-09 21:22:41 +0530

    second commit

---
+++ springwebapplication-1/src/main/java/com/srikar/springdemo/Springwebapplication1Application.java
@@ -0,0 +1,13 @@
+package com.srikar.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapplication1Application.class, args);
+	}
+
+}
---
+++ springwebapplication-1/src/test/java/com/srikar/springdemo/Springwebapplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srikar.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springwebapplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
